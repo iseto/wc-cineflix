095bd120bb850b85a08df142d9f0b502a90a2c6a
Ignatius Seto <ignatius.seto@accenture.com>
2023-05-30 11:51:51 +0200

    (feat/movie-finder): add initial styling for movie-finder

--- src/components/movie-finder/movie-finder.tsx
+++ src/components/movie-finder/movie-finder.tsx
@@ -2,7 +2,18 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'wc-movie-finder',
-  styleUrl: './movie-finder.scss',
+  styleUrl: 'movie-finder.css',
   shadow: true,
 })
-export class MovieFinder {}
+export class MovieFinder {
+  render() {
+    return [
+      <form>
+        <div class="movie-finder">
+          <input type="text" name="movie-finder-input" id="movie-finder-input" />
+          <button type="submit">Find!</button>
+        </div>
+      </form>,
+    ];
+  }
+}
--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'cineflix',
-  globalStyle: 'src/global/global.scss',
+  globalStyle: 'src/global/global.css',
   outputTargets: [
     {
       type: 'dist',
