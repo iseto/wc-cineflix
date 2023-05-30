7af98c59a762075f5788ff529a2561a9a49424c6
Ignatius Seto <ignatius.seto@accenture.com>
2023-05-30 14:38:47 +0200

    remove text

--- src/components/movie-finder/movie-finder.tsx
+++ src/components/movie-finder/movie-finder.tsx
@@ -16,8 +16,6 @@ export class MovieFinder {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTVmZjk2MjM1NDY3MTQ2NWNmY2MxNmFhNjBmZTU2ZSIsInN1YiI6IjY0NzVkYWVmMWJmMjY2MDQ0MmE3OGE3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oU13ebZk5Ra6YfNf-TWZ62-eEG8Au-QadFSXQ44-dKo',
       },
     };
 
